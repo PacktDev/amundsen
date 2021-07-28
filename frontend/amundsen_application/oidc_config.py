6c596983d8eb485ca91186a5c31fc89260b2dbcf
Ahren Stevens-Taylor <ahrent@packt.com>
2021-07-28 13:43:02 +0100

    add external server name support for load balancer

--- frontend/amundsen_application/oidc_config.py
+++ frontend/amundsen_application/oidc_config.py
@@ -40,3 +40,4 @@ def get_auth_user(app: Flask) -> User:
 class OidcConfig(LocalConfig):
     AUTH_USER_METHOD = get_auth_user
     REQUEST_HEADERS_METHOD = get_access_headers
+    SERVER_NAME = os.getenv('SERVER_NAME', 'http://localhost:5000')
